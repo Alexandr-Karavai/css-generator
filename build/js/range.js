e5c90a5e5fed6cfa86983c73bac6c81f18d8f2c3
Alexandr Karavai <karavai96-2010@mail.ru>
2018-01-17 17:50:42 +0300

    add boxShadow gen

--- build/js/range.js
+++ build/js/range.js
@@ -24,22 +24,35 @@ function setBorderRadius() {
   var borderRBR = document.getElementById('borderRBR').value;
   var borderRBL = document.getElementById('borderRBL').value;
 
-  document.getElementById('borderBlock').style
-    .borderTopLeftRadius = borderRTL + 'px';
-  document.getElementById('borderBlock').style
-    .borderTopRightRadius = borderRTR + 'px';
-  document.getElementById('borderBlock').style
-    .borderBottomRightRadius = borderRBR + 'px';
-  document.getElementById('borderBlock').style
-    .borderBottomLeftRadius = borderRBL + 'px';
+	document.getElementById('borderBlock').style
+		.borderRadius =' ' + borderRTL + 'px' + ' ' + borderRTR + 'px' +
+				' ' + borderRBR + 'px' + ' ' + borderRBL + 'px';
 
   var computedStyle = getComputedStyle(document.getElementById('borderBlock'));
   document.getElementById('resultRTL').value =
-		'border-top-left-radius: ' + computedStyle.borderTopLeftRadius + ';';
-  document.getElementById('resultRTR').value =
-		'border-top-right-radius: ' + computedStyle.borderTopRightRadius + ';';
-  document.getElementById('resultRBR').value =
-		'border-bottom-right-radius: ' + computedStyle.borderBottomRightRadius + ';';
-  document.getElementById('resultRBL').value =
-		'border-bottom-left-radius: ' + computedStyle.borderBottomLeftRadius + ';';
+		'border-radius: ' + computedStyle.borderRadius + ';';
+}
+
+function setBoxShadow() {
+  var type       = document.getElementById('typeShadow').value;
+  var horizontal = document.getElementById('typeHorizon').value;
+  var vertical   = document.getElementById('typeVertical').value;
+  var blur       = document.getElementById('typeBlur').value;
+  var spread     = document.getElementById('typeSpread').value;
+  var color      = document.getElementById('typeColor').value;
+
+  if (type == "inset")
+  {
+    document.getElementById('shadowGen').style
+      .boxShadow = type + ' ' + horizontal + 'px' + ' ' + vertical + 'px' +
+          ' ' + blur + 'px' + ' ' + spread + 'px' + color;
+  } else {
+    document.getElementById('shadowGen').style
+      .boxShadow = horizontal + 'px' + ' ' + vertical + 'px' +
+          ' ' + blur + 'px' + ' ' + spread + 'px' + color;
+  }
+
+  var computedStyle = getComputedStyle(document.getElementById('shadowGen'));
+  document.getElementById('resultShadow').value =
+		'box-shadow: ' + computedStyle.boxShadow + ';';
 }
